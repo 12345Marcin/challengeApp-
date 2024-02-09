24f755e6c7e5d51e80c36983183e47c4896319d5
Marcin Wiśniewski <marcin400@vp.pl>
2024-02-09 16:58:18 +0100

    dzień 4

--- challengeApp-/challengeApp-/Program.cs
+++ challengeApp-/challengeApp-/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, Marcin!");
+﻿string name = "Marcin";
+char sex = 'm';
+int age = 33;
+
+if (sex == 'K' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa,lat30");
+}
+else if (sex == 'm' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
